ff2169c0081f03dabdf145cc14ee7a925e9c6cbf
jaychouzzz <f18326186224@gmail.com>
2019-09-08 15:14:48 +0800

    update

---
+++ src/main/java/com/example/rebase_demo/RebaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebase_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebaseDemoApplication.class, args);
+    }
+
+}
